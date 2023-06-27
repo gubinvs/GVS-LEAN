f88a36284b1db9c92df3a7d2596f650da9b3ebe4
master <gubinvs@gmail.com>
2023-06-27 13:47:45 +0300

    create burger js

--- src/js/modules/burger-action.js
+++ src/js/modules/burger-action.js
@@ -6,16 +6,20 @@ const burgerСlose = document.querySelector('.burger_close');
 //-- Инициализировал переменную с классом .header__mobile
 const classBlockActive = document.querySelector('.nav-block-active');
 //-- Инициализировал переменную с классом .header__mobile
-const classHeaderMobile = document.querySelector('.header__mobile');
+const classBurgerClose = document.querySelector('.burger_close');
 
 // событие клик по бургеру
 burger.addEventListener("click", function(e) {
     //-- добаляю классы
     classBlockActive.className = "nav-block-active";
+    classBurgerClose.className = "burger_close";
+    burger.className = ".none-block";
 });
 
 // событие клик по бургеру крестику
 burgerСlose.addEventListener("click", function(e) {
     //-- добаляю классы
-    classBlockActive.className = "nav-block-active";
+    classBlockActive.className = "nav-block-active none-block";
+    classBurgerClose.className = "burger_close none-block";
+    burger.className = "burger";
 });
