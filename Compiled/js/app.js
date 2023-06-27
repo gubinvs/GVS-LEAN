1d27c846f930888efb51b786e6d44fd180589693
master <gubinvs@gmail.com>
2023-06-27 18:11:15 +0300

    s

--- Compiled/js/app.js
+++ Compiled/js/app.js
@@ -19,4 +19,26 @@ for (let i=1; i<item+1; i++) {
     });   
 }
 
+//-- Инициализировал переменную с крестиком
+const burgerСlose = document.querySelector('.burger_close');
+//-- Инициализировал переменную с классом .header__mobile
+const classBlockActive = document.querySelector('.nav-block-active');
+//-- Инициализировал переменную с классом .header__mobile
+const classBurgerClose = document.querySelector('.burger_close');
+
+// событие клик по бургеру
+burger.addEventListener("click", function(e) {
+    //-- добаляю классы
+    classBlockActive.className = "nav-block-active";
+    classBurgerClose.className = "burger_close";
+    burger.className = ".none-block";
+});
+
+// событие клик по бургеру крестику
+burgerСlose.addEventListener("click", function(e) {
+    //-- добаляю классы
+    classBlockActive.className = "nav-block-active none-block";
+    classBurgerClose.className = "burger_close none-block";
+    burger.className = "burger";
+});
 
